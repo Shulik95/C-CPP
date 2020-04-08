8d2bc26d0637709186c303b2f8cab95412766abe
Shalom Kachko <shalom.kachko@mail.huji.ac.il>
2020-04-08 16:03:02 +0300

    finaly figured out fgets and sscanf

--- manageStudents.c
+++ manageStudents.c
@@ -6,11 +6,15 @@
  * defines the max length for each field of the user input.
  */
 #define MAX_FIELD_LEN 42
+#define MAX_LINE_LEN 60
 #define MAX_STUDENT_NUM 5500
 #define BEST "best"
 #define MERGE "merge"
 #define QUICK "quick"
 
+/**
+ * a Student struct, enveloping all relevant student information.
+ */
 typedef struct Student{
     int ID;
     int grade;
@@ -19,18 +23,40 @@ typedef struct Student{
     char city[MAX_FIELD_LEN], name[MAX_FIELD_LEN], country[MAX_FIELD_LEN];
 }Student;
 
+int gStopFlag = 0;
 int gStudentCounter = 0;
 Student gStudentList[MAX_STUDENT_NUM];
-char gBest[4] = {'b','e','s','t','\0'};
-char gMerge[5] = {'m','e','r','g','e','\0'};
 
+/**
+ * the main running function for getting the best student, recieves input, calls needed methods to
+ * check legality and returns the student if such exits.
+ */
+void getBestStudent()
+{
+    while(gStopFlag == 0)
+    {
+        char id, grade, age;
+        int counter;
+        char input[MAX_LINE_LEN], city[MAX_FIELD_LEN], name[MAX_FIELD_LEN], country[MAX_FIELD_LEN];
+        printf("Enter student info.  To exit press q, then enter\n");
+        fgets(input, MAX_LINE_LEN , stdin);
+        sscanf(input, "%c, %[^,], %c, %c, %[^,], %[^\n]", &id, name, &grade, &age, country, city);
+        if
+    }
+    printf("i get here, good job Shulik! :)");
+}
 
+/**
+ * This function
+ * @return
+ */
+int checkInput(){
 
-
+}
 
 int main(int argc, char* argv[]) {
-    if(argv[1] == BEST){
-
+    if(strcmp(BEST,argv[1]) == 0){
+        getBestStudent();
     }
     return 0;
 }
