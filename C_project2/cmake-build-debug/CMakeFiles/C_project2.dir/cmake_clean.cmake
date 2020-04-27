d615fa4f08e2cc7d82979b44afda07b72b126a62
Shalom Kachko <shalom.kachko@mail.huji.ac.il>
2020-04-27 18:43:57 +0300

    first commit, file created

---
+++ C_project2/cmake-build-debug/CMakeFiles/C_project2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_project2.dir/main.c.o"
+  "C_project2.exe"
+  "C_project2.pdb"
+  "libC_project2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_project2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C_project2/cmake-build-debug/CMakeFiles/C_project2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for C_project2.
+# This may be replaced when dependencies are built.
