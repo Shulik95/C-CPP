5238ff5a2aa92fcedde8f545b76a11597aa10376
Shalom Kachko <shalom.kachko@mail.huji.ac.il>
2020-04-27 21:10:24 +0300

    finally got it to read from file

--- C_project2/RailWayPlanner.c
+++ C_project2/RailWayPlanner.c
@@ -4,19 +4,35 @@
 #include <stdio.h>
 #include <limits.h>
 #include <stdbool.h>
-#define MAX_LINE_LENGTH 1024
-char *gline;
+#include <string.h>
+#include <stdlib.h>
 
-void openFile(char *arr)
+const int gMaxLineLen =  1024;
+
+
+void openFile(char const *const arr) //1st const locks the values, 2nd one locks the file pointer
 {
-    FILE *file = fopen(arr, (const char *) 'r');
-    gline = fgets(gline, MAX_LINE_LENGTH, file);
-    printf("1st line is: %s", gline);
+    FILE* file = fopen(arr, "r");
+    if(file == NULL) //failed to open file for some reason
+    {
+        fprintf(stderr,"File doesn't exist\n"); /// change to print into output file~!
+        exit(EXIT_FAILURE);
+    }
+    char tempLine[gMaxLineLen];
+    fgets(tempLine, gMaxLineLen, file);
+    printf("1st tempLine is: %s", tempLine);
+    fclose(file);
 }
 
-int main(argc,(char**)argv[1])
-{
-    openFile(argv[1]);
+
+int main(int argc, char* argv[]) {
+
+    if(argc != 2)
+    {
+        fprintf(stderr,"Usage: RailWayPlanner <InputFile>"); /// change to print into output file~!
+        exit(EXIT_FAILURE);
+    }
+    char const *const fName = argv[1];
+    openFile(fName);
     return 0;
 }
-
