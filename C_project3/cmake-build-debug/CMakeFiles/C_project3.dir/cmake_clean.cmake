3cbc2a6b239d7fa601ea0934833d9faafb674474
Shalom Kachko <shalom.kachko@mail.huji.ac.il>
2020-05-25 18:23:26 +0300

    starting ex3 67315

---
+++ C_project3/cmake-build-debug/CMakeFiles/C_project3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_project3.dir/main.c.o"
+  "C_project3.exe"
+  "C_project3.pdb"
+  "libC_project3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_project3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C_project3/cmake-build-debug/CMakeFiles/C_project3.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for C_project3.
+# This may be replaced when dependencies are built.
