b2563a909c4715e880cf71dda5febbe8df8e669e
Shalom Kachko <shalom.kachko@mail.huji.ac.il>
2020-05-27 20:51:25 +0300

    copyIfNormIsLarger implemented, untested

--- C_project3/Structs.c
+++ C_project3/Structs.c
@@ -19,6 +19,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "RBTree.h"
 // -------------------------- const definitions -------------------------
 #define GREATER 1
 #define LESSER -1
@@ -121,7 +122,58 @@ void freeVector(void *pVector)
     }
     free(((Vector*)pVector)->vector); //free dynamically allocated array.
     ((Vector*)pVector)->vector = NULL;
+    free(pVector);
 }
+
+/**
+ * calculates the norm of a given vector.
+ * @param vec - pointer to a vector struct.
+ * @return - the norm of the given vector.
+ */
+double calcNorm(const Vector* vec)
+{
+    double norm = 0;
+    for(int i = 0; i < vec->len; i++)
+    {
+        norm +=(vec->vector[i] * vec->vector[i]);
+    }
+    return norm;
+}
+
+/**
+ * copy pVector to pMaxVector if : 1. The norm of pVector is greater then the norm of pMaxVector.
+ * 								   2. pMaxVector->vector == NULL.
+ * @param pVector pointer to Vector
+ * @param pMaxVector pointer to Vector that will hold a copy of the data of pVector.
+ * @return 1 on success, 0 on failure (if pVector == NULL || pMaxVector==NULL: failure).
+ */
+int copyIfNormIsLarger(const void *pVector, void *pMaxVector)
+{
+    if(pVector == NULL || pMaxVector == NULL) // no vector to copy.
+    {
+        return FAIL;
+    }
+    else if(((Vector*)pMaxVector)->vector && calcNorm(pVector) <= calcNorm(pMaxVector))
+    {
+        return CONT_SUCC;
+    }
+    else
+    {
+        /*make sure vectors are the same length before copying*/
+        ((Vector*)pMaxVector)->vector = (double*)realloc(((Vector*)pMaxVector)->vector,
+                                                         sizeof(double)*((Vector*)pVector)->len);
+        if(((Vector*)pMaxVector)->vector == NULL)
+        {
+            return FAIL;
+        }
+        ((Vector*)pMaxVector)->vector = \
+        (double*)memcpy(((Vector*)pMaxVector)->vector,((Vector*)pVector)->vector,
+                sizeof(double)*((Vector*)pVector)->len);
+        return CONT_SUCC;
+    }
+}
+
+
 
 //int main() {
 //    Vector v1, v2, v3;
