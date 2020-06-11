393e61f039e05d24acefa968ee50277597cccaa2
Shalom Kachko <shalom.kachko@mail.huji.ac.il>
2020-06-11 20:27:30 +0300

    first commit starting project

---
+++ CPP_ex1/cmake-build-debug/CMakeFiles/CPP_ex1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP_ex1.dir/main.cpp.o"
+  "CPP_ex1.exe"
+  "CPP_ex1.pdb"
+  "libCPP_ex1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP_ex1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
