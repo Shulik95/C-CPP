c14ae3ecc69581c632c1a1a173da127c2471d33b
Shalom Kachko <shalom.kachko@mail.huji.ac.il>
2020-06-12 21:46:35 +0300

    Dense done not tested

---
+++ CPP_ex1/Dense.cpp
@@ -0,0 +1,60 @@
+
+/**
+ * @file Dense.cpp
+ * @author  Shalom Kachko
+ * @date 12 june 2020
+ *
+ * @brief represents a layer of the network.
+ */
+
+// ------------------------------ includes ------------------------------
+#include "Matrix.h"
+#include "Dense.h"
+// -------------------------- const definitions -------------------------
+// ------------------------------- methods ------------------------------
+
+/**
+ * Constructor for Dense class.
+ * @param w - Matrix object holding weights.
+ * @param bias - Matrix.
+ */
+Dense::Dense(const Matrix& w, const Matrix& bias, ActivationType actType) : weights(w), bias(bias), activation(Activation(actType))
+{
+}
+
+/**
+ * @return - Returns the weights of this layer as a Matrix obj.
+ */
+const Matrix &Dense::getWeights() const
+{
+    return weights;
+}
+/**
+ * @return - Returns the bias of this layer as a Matrix.
+ */
+const Matrix &Dense::getBias() const
+{
+    return bias;
+}
+
+/**
+ * @return - the Activation obj of the layer.
+ */
+const Activation &Dense::getActivation() const {
+    return activation;
+}
+
+/**
+ * Applies the layer on inputed Matrix.
+ * @param mat - input to
+ * @return
+ */
+Matrix Dense::operator()(const Matrix &mat)
+{
+    Matrix ret = Matrix(mat);
+    ret = (this->weights * ret) + this->bias; // W1*x +b1
+    ret = this->activation(ret); // Relu/Softmax(W1*x +b1)
+    return ret;
+}
+
+
--- CPP_ex1/Dense.h
+++ CPP_ex1/Dense.h
@@ -1,8 +1,48 @@
-//
-// Created by user on 11-Jun-20.
-//
+
 
 #ifndef CPP_EX1_DENSE_H
 #define CPP_EX1_DENSE_H
+
+#include "Matrix.h"
+#include "Activation.h"
+
+class Dense
+{
+private:
+    Matrix weights;
+    Matrix bias;
+    Activation activation;
+
+public:
+
+    /**
+     * Constructor for Dense class.
+     * @param w - Matrix object holding weights.
+     * @param bias - Matrix.
+     */
+    Dense(const Matrix& w, const Matrix& bias, ActivationType);
+
+    /**
+     * @return - Returns the weights of this layer as a Matrix obj.
+     */
+    const Matrix &getWeights() const;
+
+    /**
+     * @return - Returns the bias of this layer as a Matrix.
+     */
+    const Matrix &getBias() const;
+
+    /**
+     * @return - the Activation obj of the layer.
+     */
+    const Activation &getActivation() const;
+
+    /**
+     * Applies the layer on inputed Matrix.
+     * @param mat - input to
+     * @return
+     */
+    Matrix operator()(const Matrix& mat);
+};
 
 #endif //CPP_EX1_DENSE_H
