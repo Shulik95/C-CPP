84d222c90eee061731d2129bedfa471d38ba93ea
Shulik95 <51851511+Shulik95@users.noreply.github.com>
2020-08-02 21:23:57 +0300

    manual upload #1 cause git hates me
    binary seach done
    comparator done

--- c_exam/SpreaderDetectorBackend.c
+++ c_exam/SpreaderDetectorBackend.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include "SpreaderDetectorParams.h"
 #include <string.h>
+#include <stdbool.h>
 
 // ----------------------------- definitions ----------------------------
 const char* USAGE_ERR = "Usage: ./SpreaderDetectorBackend <Path to People.in> <Path to Meetings.in>\n";
@@ -15,8 +16,10 @@ const int LEGAL_INPUT_NUM = 3;
 #define MAX_LINE_LEN 1024
 #define BASE_SIZE 10
 #define DEFAULT_PROB 0.0
+#define FAILURE -1
 
 int gCounter = 0;
+bool meetingFlag = false;
 
 /**
  * struct representing each person.
@@ -32,11 +35,12 @@ typedef struct Person
  * struct to serve as weighted edges in graph, the weight is determined by time an distance the infected has spent with
  * the infector.
  */
-typedef struct Edge
-{
-    Person src, dest;
-    float time, distance;
-} Edge;
+//typedef struct Edge
+//{
+//    Person src, dest;
+//    float time, distance;
+//} Edge;
+
 
 /**
  * input file and output file, will be initialized in main.
@@ -75,11 +79,24 @@ void addPerson(char* line, Person** arr ,int lineCounter)
 }
 
 /**
- * this function parses the people.in file.
+ * comparator func for qsort.
+ * @param p1 - first person to compare.
+ * @param p2 - 2nd person to compare.
+ * @return - return val is positive if p1 > p2, negative if p2 > p1, 0 if they're equal.
+ */
+int cmpfunc(const void* p1, const void* p2)
+{
+    int num1 = (int)(*(Person*)p1).ID;
+    int num2 = (int)(*(Person*)p2).ID;
+    return (num1 - num2);
+}
+
+/**
+ * this function parses the people.in file, allocates memory but doesnt free it!
  * @param fileName - name of file to parse.
  * @return - an array containing Person structs for all the people.
  */
-Person* parsePeopleFile(const char* const fileName)
+void* parsePeopleFile(const char* const fileName)
 {
     int capacity = BASE_SIZE;
     int lineCounter = 0;
@@ -91,47 +108,87 @@ Person* parsePeopleFile(const char* const fileName)
         printError(INPUT_ERR);
     }
 
-    Person* personArr = (Person*)malloc(capacity * sizeof(Person));
-    if(personArr == NULL) // malloc failed, close files and exit program.
+    char buffer[MAX_LINE_LEN];
+    Person* arr = (Person*)malloc(capacity * sizeof(Person));
+    if(arr == NULL) // malloc failed, close files and exit program.
     {
         printError(STL_ERR);
     }
-
-    char buffer[MAX_LINE_LEN];
     while(fgets(buffer, MAX_LINE_LEN, inputFile) != NULL)
     {
         if (lineCounter == capacity) //increase array if needed.
         {
             capacity += BASE_SIZE;
-            personArr = (Person*) realloc(personArr, sizeof(Person) * capacity);
-            if(personArr == NULL)
+            arr = (Person*) realloc(arr, sizeof(Person) * capacity);
+            if(arr == NULL)
             {
-                free(personArr); //free what was already allocated.
+                free(arr); //free what was already allocated.
                 printError(STL_ERR); //close files and exit.
             }
         }
-        addPerson(buffer, &personArr ,lineCounter);
+        addPerson(buffer, &arr , lineCounter);
         lineCounter++;
         gCounter++;
     }
-    return personArr;
+    fclose(inputFile);
+    return arr;
 }
+
+/**
+ * returns the index of the person struct with the given ID number.
+ * @param arr - sorted array of Person structs.
+ */
+int binarySearch(Person arr[], int left, int right, int val)
+{
+    if(right >= left)
+    {
+        int mid = (left + right) / 2;
+        int midVal = (int)arr[mid].ID;
+        if(midVal == val)
+        {
+            return mid; //found value, return it
+        }
+        else if(val < midVal) //val must be on the left of mid
+        {
+            return binarySearch(arr, left, mid + 1, val);
+        }
+        else //val is on the right
+        {
+            return binarySearch(arr, mid + 1, right, val);
+        }
+    }
+    return FAILURE;
+}
+
+/**
+ * parses the "meeting.in" file. calculates the infection probability and adds it to the correct Person
+ * struct.
+ * @param fileName - file to parse.
+ * @param arr - array of Person structs.
+ * @return - pointer to an array of Person structs.
+ */
+Person* finalizeData(const char* fileName, Person** arr)
+{
+
+
+}
+
 
 
 int main(int argc, char* argv[])
 {
     if (argc != LEGAL_INPUT_NUM)
     {
-        fprintf(stderr, "%s", USAGE_ERR);
-        exit(EXIT_FAILURE);
+        printError(USAGE_ERR);
     }
     outputFile = fopen(gOutputName, "w");
-    if(outputFile == NULL)
+    if(outputFile == NULL) //opening outputFile failed
     {
-
+        printError(OUTPUT_ERR);
     }
     const char* const peopleFile = argv[1];
-    Person* temp = parsePeopleFile(peopleFile);
+    Person* temp = (Person*)parsePeopleFile(peopleFile);
+    const char* meetingFile = argv[2];
     for(int i = 0; i < gCounter; i++ )
     {
         fprintf(outputFile, "Name: %s, ID: %s, age: %f \n", temp[i].name, temp[i].ID, temp[i].age);
