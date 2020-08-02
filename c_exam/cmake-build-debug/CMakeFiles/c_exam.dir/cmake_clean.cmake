d6c2b544b2d38062e8f9a419bac67f3239d7d7a5
Shalom Kachko <shalom.kachko@mail.huji.ac.il>
2020-08-02 10:02:26 +0300

    first commit starting project

---
+++ c_exam/cmake-build-debug/CMakeFiles/c_exam.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_exam.dir/main.cpp.o"
+  "c_exam.exe"
+  "c_exam.pdb"
+  "libc_exam.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c_exam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
