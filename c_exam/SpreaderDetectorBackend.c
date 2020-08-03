574833453d768727e2e3c054627493b8b2618884
Shulik95 <51851511+Shulik95@users.noreply.github.com>
2020-08-03 11:32:43 +0300

    inserting into data struct method done.
    manual upload #3

--- c_exam/SpreaderDetectorBackend.c
+++ c_exam/SpreaderDetectorBackend.c
@@ -3,14 +3,11 @@
 #include <stdlib.h>
 #include "SpreaderDetectorParams.h"
 #include <string.h>
-#include <stdbool.h>
 
 // ----------------------------- definitions ----------------------------
 const char* USAGE_ERR = "Usage: ./SpreaderDetectorBackend <Path to People.in> <Path to Meetings.in>\n";
 const char* INPUT_ERR = "Error in input files.\n";
 const char* OUTPUT_ERR = "Error in output file.\n";
-const char* STL_ERR = "Standard library error.\n";
-const char* gOutputName = "SpreaderDetectorAnalysis.out";
 const int LEGAL_INPUT_NUM = 3;
 
 #define MAX_LINE_LEN 1024
@@ -18,9 +15,10 @@ const int LEGAL_INPUT_NUM = 3;
 #define DEFAULT_PROB 0.0
 #define FAILURE -1
 #define SICK 1
+#define GT 1
+#define EQUAL 0
 
 int gCounter = 0;
-bool meetingFlag = false;
 
 /**
  * struct representing each person.
@@ -31,17 +29,6 @@ typedef struct Person
     char name[MAX_LINE_LEN], ID[MAX_LINE_LEN];
     float age, prob;
 } Person;
-
-/**
- * struct to serve as weighted edges in graph, the weight is determined by time an distance the infected has spent with
- * the infector.
- */
-//typedef struct Edge
-//{
-//    Person src, dest;
-//    float time, distance;
-//} Edge;
-
 
 /**
  * input file and output file, will be initialized in main.
@@ -85,7 +72,7 @@ void addPerson(char* line, Person** arr ,int lineCounter)
  * @param p2 - 2nd person to compare.
  * @return - return val is positive if p1 > p2, negative if p2 > p1, 0 if they're equal.
  */
-int cmpfunc(const void* p1, const void* p2)
+int idCmpfunc(const void* p1, const void* p2)
 {
     int num1 = (int)(*(Person*)p1).ID;
     int num2 = (int)(*(Person*)p2).ID;
@@ -113,7 +100,7 @@ void* parsePeopleFile(const char* const fileName)
     Person* arr = (Person*)malloc(capacity * sizeof(Person));
     if(arr == NULL) // malloc failed, close files and exit program.
     {
-        printError(STL_ERR);
+        printError(STANDARD_LIB_ERR_MSG);
     }
     while(fgets(buffer, MAX_LINE_LEN, inputFile) != NULL)
     {
@@ -124,7 +111,7 @@ void* parsePeopleFile(const char* const fileName)
             if(arr == NULL)
             {
                 free(arr); //free what was already allocated.
-                printError(STL_ERR); //close files and exit.
+                printError(STANDARD_LIB_ERR_MSG); //close files and exit.
             }
         }
         addPerson(buffer, &arr , lineCounter);
@@ -136,9 +123,20 @@ void* parsePeopleFile(const char* const fileName)
 }
 
 /**
+ * calculates the the probability of one person infecting another.
+ * @param time - time they spent together.
+ * @param distance - distance between the two people.
+ */
+float Crna(float const time, float const distance)
+{
+    return (time * MIN_DISTANCE) / (distance * MAX_TIME);
+}
+
+/**
  * returns the index of the person struct with the given ID number.
  * @param arr - sorted array of Person structs.
  */
+ //TODO: test binaetSeach first thing tommorow!!!
 int binarySearch(Person arr[], int left, int right, int val)
 {
     if(right >= left)
@@ -161,6 +159,8 @@ int binarySearch(Person arr[], int left, int right, int val)
     return FAILURE;
 }
 
+
+
 /**
  * parses the "meeting.in" file. calculates the infection probability and adds it to the correct Person
  * struct.
@@ -174,23 +174,70 @@ Person* finalizeData(const char* fileName, Person** arr)
     int id1, id2, currInfectorIdx;
     char* res;
     Person* personArr = *arr;
+
     inputFile = fopen(fileName, "w");
     if(inputFile == NULL)
     {
         /*opening file failed, free memory and close files.*/
         free(personArr);
-        printError(STL_ERR);
+        printError(STANDARD_LIB_ERR_MSG);
     }
-    qsort(personArr, gCounter, sizeof(Person), cmpfunc); //sort array.
+
+    qsort(personArr, gCounter, sizeof(Person), idCmpfunc); //sort array to use binary search.
     char buffer[MAX_LINE_LEN];
     fgets(buffer, MAX_LINE_LEN, inputFile); //get first line
     int tempID = (int)strtol(buffer, &res, 10);
     int idx = binarySearch(personArr, 0, gCounter - 1, tempID);
     personArr[idx].prob = SICK;
+    currInfectorIdx = idx;
     while(fgets(buffer, MAX_LINE_LEN, inputFile) != NULL)
     {
-
+        sscanf(buffer, "%d %d %f %f", &id1, &id2, &dist, &time);
+        if(currInfectorIdx != binarySearch(personArr, 0, gCounter - 1, id1)) //infector changed, find new one.
+        {
+            currInfectorIdx = binarySearch(personArr, 0, gCounter - 1, id1); // get correct infector index.
+        }
+        float currCrna = Crna(time, dist);
+        currCrna *= personArr[currInfectorIdx].prob;
+        int infectedIdx = binarySearch(personArr, 0, gCounter, id2); // get 2nd person struct.
+        personArr[infectedIdx].prob = currCrna; // update that struct to contain the infection probability.
     }
+    return personArr;
+}
+
+/**
+ * comparator method for sorting by infection probability.
+ * @param p1 - pointer to Person struct.
+ * @param p2 - pointer to Person struct.
+ */
+int probCompFunc(void* const p1, void* const p2)
+{
+    float prob1 = (*(Person*)p1).prob;
+    float prob2 = (*(Person*)p2).prob;
+    float res = prob1 - prob2;
+    if(res > 0)
+    {
+        return GT;
+    }
+    else if (res < 0)
+    {
+        return FAILURE;
+    }
+    else
+    {
+        return EQUAL;
+    }
+}
+
+/**
+ * iterates over given array, checks for each person the probability of infection and writes recommendation into file
+ * according to given care standards. This method is in charge of freeing the allocated array.
+ * @param arr - array of Person structs, the probability for each is calculated, array is not sorted according to
+ * probability.
+*/
+void writeDataToOutput(Person* arr)
+{
+
 }
 
 
@@ -201,7 +248,7 @@ int main(int argc, char* argv[])
     {
         printError(USAGE_ERR);
     }
-    outputFile = fopen(gOutputName, "w");
+    outputFile = fopen(OUTPUT_FILE, "w");
     if(outputFile == NULL) //opening outputFile failed
     {
         printError(OUTPUT_ERR);
