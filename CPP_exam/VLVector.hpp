fe8a3bb902f064b65cda827ca808ed3b08c9743c
Shalom Kachko <shalom.kachko@mail.huji.ac.il>
2020-08-04 18:15:15 +0300

    starting project first push

---
+++ CPP_exam/VLVector.hpp
@@ -0,0 +1,8 @@
+//
+// Created by user on 04-Aug-20.
+//
+
+#ifndef CPP_EXAM_VLVECTOR_HPP
+#define CPP_EXAM_VLVECTOR_HPP
+
+#endif //CPP_EXAM_VLVECTOR_HPP
---
+++ CPP_exam/cmake-build-debug/CMakeFiles/CPP_exam.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP_exam.dir/main.cpp.o"
+  "CPP_exam.exe"
+  "CPP_exam.pdb"
+  "libCPP_exam.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP_exam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
