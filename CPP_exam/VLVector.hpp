3579cd228ae9602d7d324016908727b1852ed15e
Shalom Kachko <shalom.kachko@mail.huji.ac.il>
2020-08-04 19:28:08 +0300

    implementing simple methods

--- CPP_exam/VLVector.hpp
+++ CPP_exam/VLVector.hpp
@@ -5,4 +5,70 @@
 #ifndef CPP_EXAM_VLVECTOR_HPP
 #define CPP_EXAM_VLVECTOR_HPP
 
+#include <w32api/rpcndr.h>
+
+#define DEFAULT_SIZE 16
+#define DEFAULT_AMOUNT 0
+
+template <class T, int statSize = DEFAULT_SIZE>
+class VLVector
+{
+
+private:
+    T _staticArr[statSize];
+    size_t _currSize, _currCap;
+    T* _dynamicArr;
+
+public:
+
+    /**
+     * creates a default empty VLVector with the given generic size.
+     */
+    explicit VLVector() : _currSize(DEFAULT_AMOUNT), _currCap(statSize) {}
+
+
+    /**
+     * @return - the current number of elements in the vector.
+     */
+    size_t size() const
+    {
+        return this->_currSize;
+    }
+
+    /**
+     * @return - the current capacity of the vector.
+     */
+    size_t capacity() const
+    {
+        return this->_currCap;
+    }
+
+    /**
+     * @return - true if the size is 0 false else.
+     */
+    bool empty() const
+    {
+        return this->_currSize == 0;
+
+    }
+
+    /**
+     * @param idx - index to get.
+     * @return
+     */
+    T at(int const idx) const
+    {
+        if(this->_currCap < statSize) //using static memory.
+        {
+            return this->_staticArr[idx];
+        }
+        else //currently using dynamically allocated memory
+        {
+            return this->_dynamicArr[idx];
+        }
+    }
+
+
+};
+
 #endif //CPP_EXAM_VLVECTOR_HPP
